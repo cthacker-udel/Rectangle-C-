25ca61270159804f4712a8a1f488205701f98938
cthacker-udel <cthacker@udel.edu>
2021-05-19 05:27:03 -0400

    Added run configurations, and also tests using the no-arg and 2arg Rectangle constructor

--- main.cpp
+++ main.cpp
@@ -5,12 +5,32 @@
  *      Author: flyin
  */
 
+#include <iostream>
+#include <stdlib.h>
+#include "Rect.hpp"
 
+using namespace std;
 
 int main(void){
 
 	/*****************************/
 	// Creating Rectangle instance
+
+	Rect r; // No-arg constructor instance
+
+	cout << "r's area equals : " << r.getArea() << endl;
+	cout << "r's length equals : " << r.getLength() << endl;
+	cout << "r's width equals : " << r.getWidth() << endl;
+
+	Rect r2(10,20);
+
+	cout << "r2's area equals : " << r2.getArea() << endl;
+
+	r2.setLength(50);
+
+	cout << "r2's area equals : " << r2.getArea() << endl;
+
+
 
 
 }
